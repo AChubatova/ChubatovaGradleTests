744152a3cbf6314daf2381735d9f900f70779299
AChubatova <64786092+AChubatova@users.noreply.github.com>
2021-04-21 14:54:38 +0200

    Update mytest2.java

--- src/test/java/my/nastasia.chubatova/mytest2.java
+++ src/test/java/my/nastasia.chubatova/mytest2.java
@@ -12,7 +12,7 @@ public class mytest2 {
         if (1==1){
             try
             {
-                Thread.sleep(30000);
+                Thread.sleep(3);
             }
             catch(InterruptedException e)
             {
@@ -24,7 +24,7 @@ public class mytest2 {
             System.out.println(System.currentTimeMillis());
             try
             {
-                Thread.sleep(40000);
+                Thread.sleep(4);
             }
             catch(InterruptedException e)
             {
